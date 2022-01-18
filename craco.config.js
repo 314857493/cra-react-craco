b1470bf7f118bd2b0699cb105dbc74909c260d8e
jiaxingdong <jiaxingdong@jd.com>
2022-01-18 17:41:20 +0800

    update 增加打包分析

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,9 @@
 const CracoLessPlugin = require("craco-less");
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   devServer: {
@@ -39,13 +42,33 @@ module.exports = {
       "@": path.resolve(__dirname, "./src"),
     },
     configure: (webpackConfig, { env, paths }) => {
+      const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE
+        ? "server"
+        : "json";
+
       const customConfig = {
+        plugins: [
+          ...webpackConfig.plugins,
+          new BundleAnalyzerPlugin({ analyzerMode }),
+        ],
         optimization: {
           minimize: true,
           minimizer: [
             new TerserPlugin({
+              parallel: true,
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                  drop_debugger: true,
+                  pure_funcs: ["console.log", "console.info", "console.warn"],
+                },
+                format: {
+                  comments: false,
+                },
+              },
               extractComments: false,
             }),
+            new CssMinimizerPlugin(),
           ],
         },
       };
